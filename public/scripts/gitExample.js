52a4fe32c3b66d56bdeba39a7a42cadab3033bf9
devjanaprime <dev@primeacademy.io>
2017-02-02 14:35:36 -0600

    added second field

--- public/scripts/gitExample.js
+++ public/scripts/gitExample.js
@@ -1,11 +1,20 @@
 var theList = [];
 
 var addToList = function(){
-  theList.push( document.getElementById( 'nameIn' ).value );
+  // get user input and put into an object
+  var newItem = {
+    name: document.getElementById( 'nameIn' ).value,
+    description: document.getElementById( 'descriptionIn' ).value
+  }
+  // push into list
+  theList.push( newItem );
+  // empty output
   document.getElementById( 'outputList' ).innerHTML = '';
   var outputString = '';
+  // loop through array and add to outputString
   for (var i = 0; i < theList.length; i++) {
-    outputString += '<li>' + theList[i] + '</li>';
+    outputString += '<li>' + theList[i].name + ": " + theList[i].description + '</li>';
   } //end for
+  // display output
   document.getElementById( 'outputList' ).innerHTML = outputString;
 } // end addToList
